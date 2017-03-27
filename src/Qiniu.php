b31e97f0d100f9d10cd61e0623e26c45e05b7239
ZsyD <1251992018@qq.com>
2017-03-27 13:13:56 +0800

    先上传看一下

---
+++ src/Qiniu.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace ZsyD\QiniuStream;
+
+use Qiniu\Auth;
+use Qiniu\Storage\BucketManager;
+use Qiniu\Storage\UploadManager;
+
+class Qiniu
+{
+    public static $error;
+
+    protected static $auth;
+
+    protected static $bucket;
+
+    protected static $bucketManager;
+
+    protected static $_wrapperClients = [];
+
+    public function __construct($accessKey, $secretKey)
+    {
+        if (!$this->auth($accessKey, $secretKey)) {
+
+            return false;
+        }
+
+        self::$bucketManager = new BucketManager(self::$auth);
+
+        return $this;
+    }
+
+    /**
+     * 获取空间名称 bucket
+     *
+     * @return $this
+     */
+    public static function getBucket()
+    {
+        return static::$bucket;
+    }
+
+    /**
+     * 设置空间名称 bucket
+     *
+     * @param $bucket
+     * @return $this
+     */
+    public function setBucket($bucket)
+    {
+        static::$bucket = $bucket;
+
+        return $this;
+    }
+
+    public function auth($accessKey, $secretKey)
+    {
+        if (!self::$auth) {
+            if (empty($accessKey)) {
+
+                self::$error = '请传入accessKey';
+
+                return false;
+            }
+            if (empty($secretKey)) {
+
+                self::$error = '请传入secretKey';
+
+                return false;
+            }
+
+            self::$auth = new Auth($accessKey, $secretKey);
+        }
+
+        return self::$auth;
+    }
+
+
+    /**
+     * 注册自定义类的封装协议
+     *
+     * @param $name
+     */
+    public function registerStreamWrapper($name)
+    {
+        stream_register_wrapper($name, "ZsyD\\QiniuStream\\QiniuStream");
+        $this->registerAsClient($name);
+    }
+
+    /**
+     * 注销自定义类的封装协议
+     *
+     * @param $name
+     */
+    public function unRegisterStreamWrapper($name)
+    {
+        stream_wrapper_unregister($name);
+        $this->unRegisterAsClient($name);
+    }
+
+    /**
+     * 定义封装协议
+     *
+     * @param $name
+     * @return $this
+     */
+    public function registerAsClient($name)
+    {
+        self::$_wrapperClients[$name] = $this;
+
+        return $this;
+    }
+
+    /**
+     * 删除指定名称的封装协议
+     *
+     * @param $name
+     * @return $this
+     */
+    public function unRegisterAsClient($name)
+    {
+        unset(self::$_wrapperClients[$name]);
+
+        return $this;
+    }
+
+    /**
+     * 获取指定名称的封装协议
+     *
+     * @param $name
+     * @return mixed
+     */
+    public static function getWrapperClient($name)
+    {
+        return self::$_wrapperClients[$name];
+    }
+
+    /**********************功能**************************/
+
+    /**
+     * 获取账号下所有空间名
+     *
+     * @return bool|\string[]
+     */
+    public function lists()
+    {
+        return self::$bucketManager->buckets();
+    }
+
+    public function filesList()
+    {
+        return self::$bucketManager->listFiles(self::$bucket);
+    }
+
+    public function fileStat($key)
+    {
+        return self::$bucketManager->stat(self::$bucket, $key);
+    }
+
+    public function fileRename($oldName, $newName)
+    {
+        return self::$bucketManager->rename(self::$bucket, $oldName, $newName);
+    }
+
+    public function upload($filePath, $key,  array $option)
+    {
+        $uptoken = self::$auth->uploadToken(self::$bucket, $option['key'], $option['expires'], $option['policy']);
+        $uploadMgr = new UploadManager();
+        // 调用 UploadManager 的 putFile 方法进行文件的上传。
+        list($ret, $err) = $uploadMgr->putFile($uptoken, $key, $filePath);
+        echo "\n====> putFile result: \n";
+        if ($err !== null) {
+            var_dump($err);
+        } else {
+            var_dump($ret);
+        }
+    }
+
+    public function stat($key)
+    {
+        list($ret, $err) = self::$bucketManager->stat(self::$bucket, $key);
+        if ($err !== null) {
+            var_dump($err);
+        } else {
+            var_dump($ret);
+        }
+    }
+
+
+
+
+}
---
+++ src/QiniuStream.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace ZsyD\QiniuStream;
+
+use Exception;
+use Medz\Component\WrapperInterface\WrapperInterface;
+
+class QiniuStream implements WrapperInterface
+{
+    private $_writeBuffer = false;
+
+    private $_position = 0;
+
+    private $_objectSize = 0;
+
+    private $_objectName = null;
+
+    private $_objectBuffer = null;
+
+    private $_bucketList = [];
+
+    private $_oss = null;
+
+    public $error;
+
+    /**
+     * Extract object name from URL.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function _getNamePart($path)
+    {
+        $url = parse_url($path);
+        if ($url['host']) {
+            return !empty($url['path']) ? $url['host'].$url['path'] : $url['host'];
+        }
+
+        return '';
+    }
+
+    protected function _getOssClient($path)
+    {
+        if ($this->_oss === null) {
+            $url = explode(':', $path);
+
+            if (empty($url)) {
+                throw new Exception("Unable to parse URL $path");
+            }
+
+            $this->_oss = Qiniu::getWrapperClient($url[0]);
+
+            if (!$this->_oss) {
+                throw new Exception("Unknown client for wrapper {$url[0]}");
+            }
+        }
+
+        return $this->_oss;
+    }
+
+    public function stream_open($path, $mode, $options, &$opened_path)
+    {
+        $name = $this->_getNamePart($path);
+        if (strpbrk($mode, 'wax')) {
+            $this->_objectName = $name;
+            $this->_objectBuffer = null;
+            $this->_objectSize = 0;
+            $this->_position = 0;
+            $this->_writeBuffer = true;
+            $this->_getOssClient($path);
+
+            return true;
+        } else {
+            dump(9);exit;
+            $info = $this->_getOssClient($path)->getObjectMeta(Qiniu::getBucket(), $name);
+            if ($info) {
+                $this->_objectName = $name;
+                $this->_objectBuffer = null;
+                $this->_objectSize = (int) $info['content-length'];
+                $this->_position = 0;
+                $this->_writeBuffer = false;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function dir_opendir($path, $options)
+    {
+        $dirName = $this->_getNamePart($path).'/';
+        if (preg_match('@^([a-z0-9+.]|-)+://$@', $path) || $dirName == '/') {
+            $list = $this->_getOssClient($path)->listFiles(Qiniu::getBucket());
+        } else {
+            $list = $this
+                ->_getOssClient($path)
+                ->listFiles(Qiniu::getBucket(), $dirName);
+        }
+
+        foreach ((array) $list->getPrefixList() as $l) {
+            array_push($this->_bucketList, basename($l->getPrefix()));
+        }
+
+        foreach ((array) $list->getObjectList() as $l) {
+            if ($l == $dirName) {
+                continue;
+            }
+
+            array_push($this->_bucketList, basename($l->getKey()));
+        }
+
+        return $this->_bucketList !== false;
+    }
+
+    public function dir_closedir()
+    {
+
+    }
+
+    public function dir_readdir()
+    {
+
+    }
+
+    public function dir_rewinddir()
+    {
+
+    }
+
+    public function mkdir($path, $mode, $options)
+    {
+
+    }
+
+    public function rename($path_from, $path_to)
+    {
+
+    }
+
+    public function rmdir($path, $options)
+    {
+
+    }
+
+    public function stream_cast($cast_as)
+    {
+
+    }
+
+    public function stream_close()
+    {
+
+    }
+
+    public function stream_eof()
+    {
+
+    }
+
+    public function stream_flush()
+    {
+
+    }
+
+    public function stream_lock($operation)
+    {
+
+    }
+
+
+
+    public function stream_read($count)
+    {
+        if (!$this->_objectName) {
+            return '';
+        }
+        // make sure that count doesn't exceed object size
+        if ($count + $this->_position > $this->_objectSize) {
+            $count = $this->_objectSize - $this->_position;
+        }
+
+        $range_start = $this->_position;
+        $range_end = $this->_position + $count;
+
+        // Only fetch more data from OSS if we haven't fetched any data yet (postion=0)
+        // OR, the range end position is greater than the size of the current object
+        // buffer AND if the range end position is less than or equal to the object's
+        // size returned by OSS
+        if (($this->_position == 0) || (($range_end > strlen($this->_objectBuffer)) && ($range_end <= $this->_objectSize))) {
+            $options = [
+                OssClient::OSS_RANGE => $range_start.'-'.$range_end,
+            ];
+            $this->_objectBuffer .= $this->_oss->get(Qiniu::getBucket(), $this->_objectName, $options);
+        }
+
+        $data = substr($this->_objectBuffer, $this->_position, $count);
+        $this->_position += strlen($data);
+
+        return $data;
+
+        $ret = substr($GLOBALS[$this->_objectName], $this->_objectName, $count);
+        $this->_position += strlen($ret);
+        return $ret;
+    }
+
+    public function stream_seek($offset, $whence = SEEK_SET)
+    {
+
+    }
+
+    public function stream_set_option($option, $arg1, $arg2)
+    {
+
+    }
+
+    public function stream_stat()
+    {
+        if (!$this->_objectName) {
+            return [];
+        }
+
+        $stat = [];
+        $stat['dev'] = 0;
+        $stat['ino'] = 0;
+        $stat['mode'] = 0777;
+        $stat['nlink'] = 0;
+        $stat['uid'] = 0;
+        $stat['gid'] = 0;
+        $stat['rdev'] = 0;
+        $stat['size'] = 0;
+        $stat['atime'] = 0;
+        $stat['mtime'] = 0;
+        $stat['ctime'] = 0;
+        $stat['blksize'] = 0;
+        $stat['blocks'] = 0;
+
+        if (($slash = strstr($this->_objectName, '/')) === false || $slash == strlen($this->_objectName) - 1) {
+            /* bucket */
+            $stat['mode'] |= 040000;
+        } else {
+            $stat['mode'] |= 0100000;
+        }
+
+        return $stat;
+        $info = $this->_oss->getObjectMeta(AliyunOSS::getBucket(), $this->_objectName);
+        $info = $info['_info'];
+        if (!empty($info['_info'])) {
+            $stat['size'] = $info['download_content_length'];
+            $stat['atime'] = time();
+            $stat['mtime'] = $info['filetime'];
+        }
+
+        return $stat;
+    }
+
+    public function stream_tell()
+    {
+
+    }
+
+    public function stream_write($data)
+    {
+
+    }
+
+    public function unlink($path)
+    {
+
+    }
+
+    public function url_stat($path, $flags)
+    {
+
+    }
+}
